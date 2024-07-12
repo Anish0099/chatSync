ca66bd6127d4edf5d96e8f85b2e40bc804814c12
Anish Kumar <kanish92729@gmail.com>
2024-07-12 12:06:36 +0530

    layout change

--- src/app/auth/AuthButtons.tsx
+++ src/app/auth/AuthButtons.tsx
@@ -1,24 +1,27 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 import { useState } from "react";
 
 const AuthButtons = () => {
-	const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-	return (
-		<div className='flex gap-3 flex-1 md:flex-row flex-col relative z-50'>
-			<RegisterLink className='flex-1' onClick={() => setIsLoading(true)}>
-				<Button className='w-full' variant={"outline"} disabled={isLoading}>
-					Sign up
-				</Button>
-			</RegisterLink>
-			<LoginLink className='flex-1' onClick={() => setIsLoading(true)}>
-				<Button className='w-full' disabled={isLoading}>
-					Login
-				</Button>
-			</LoginLink>
-		</div>
-	);
+  return (
+    <div className="flex flex-col justify-center gap-2">
+      <RegisterLink className="flex-1" onClick={() => setIsLoading(true)}>
+        <Button className="w-full" variant={"outline"} disabled={isLoading}>
+          Sign up
+        </Button>
+      </RegisterLink>
+      <LoginLink className="flex-1" onClick={() => setIsLoading(true)}>
+        <Button className="w-full" disabled={isLoading}>
+          Login
+        </Button>
+      </LoginLink>
+    </div>
+  );
 };
 export default AuthButtons;
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -9,25 +9,12 @@ const page = async () => {
   if (await isAuthenticated()) return redirect("/");
 
   return (
-    <div className="flex h-screen w-full">
-      <div
-        className="flex-1 flex overflow-hidden dark:bg-[#651c2b55] bg-[#651c2b] relative 
-      justify-center items-center"
-      >
-        <div className="flex flex-col gap-2 px-4 xl:ml-40 text-center md:text-start font-semibold">
-          <div className="text-3xl bg-gradient-to-r from-pink-500 to-violet-500 p-2 text-black rounded-sm">
-            Chat Sync
-          </div>
-          <AuthButtons />
+    <div className="flex h-screen justify-center items-center w-screen ">
+      <div className=" justify-center items-center gap-2 flex-col">
+        <div className="text-3xl bg-slate-900 mb-2 text-white p-2 rounded-lg">
+          ChatSync..🗨️
         </div>
-      </div>
-      <div className="flex-1 relative overflow-hidden justify-center items-center hidden md:flex bg-noise">
-        <Image
-          src={"/hero-right.jpg"}
-          alt="Hero Image"
-          fill
-          className="object-cover dark:opacity-60 opacity-90 pointer-events-none select-none h-full"
-        />
+        <AuthButtons />
       </div>
     </div>
   );
